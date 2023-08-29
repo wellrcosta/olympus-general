9c043bca388e81a27b4efc62c9ef5bd78d6ed79b
Wellington Reis <wellington.costa.97@gmail.com>
2023-08-29 17:20:12 -0300

    update/sheet-controller

--- src/controllers/sheets-controller.js
+++ src/controllers/sheets-controller.js
@@ -1,37 +1,90 @@
-const sheets_service = require('../services/sheets-service');
+/* eslint-disable indent */
+const SheetService = require('../services/sheets-service');
 
 class SheetController {
     async saveRow(req, res) {
-        const { body } = req;
-        const newRow = await sheets_service.saveRow(0, body);
-        res.json(newRow);
+        const { spreadsheetId, pageName, newData } = req.body;
+        const sheets_service = new SheetService(spreadsheetId, pageName);
+        const saveData = await sheets_service.saveData(newData);
+        switch (saveData.status) {
+            case 'success':
+                res.status(201).json(saveData.message);
+                break;
+            case 'error':
+                res.status(500).json(saveData.message);
+                break;
+            default:
+                res.status(404).json(saveData.message);
+                break;
+        }
     }
 
-    async getAllRows(_, res) {
-        const rows = await sheets_service.getAllRows(0);
-        res.json(rows);
+    async getAllRows(req, res) {
+        const { spreadsheetId, pageName } = req.body;
+        const sheets_service = new SheetService(spreadsheetId, pageName);
+        const getAllRows = await sheets_service.getAllRows();
+        switch (getAllRows.status) {
+            case 'success':
+                res.status(200).json(getAllRows.message);
+                break;
+            case 'error':
+                res.status(500).json(getAllRows.message);
+                break;
+            default:
+                res.status(404).json(getAllRows.message);
+                break;
+        }
     }
 
     async getRow(req, res) {
-        const { Identity } = req.body;
-        const row = await sheets_service.getRow(0, Identity);
-        res.json(row);
+        const { spreadsheetId, pageName, param, value } = req.body;
+        const sheets_service = new SheetService(spreadsheetId, pageName);
+        const getRow = await sheets_service.getRow(param, value);
+        switch (getRow.status) {
+            case 'success':
+                res.status(200).json(getRow.message);
+                break;
+            case 'error':
+                res.status(500).json(getRow.message);
+                break;
+            default:
+                res.status(404).json(getRow.message);
+                break;
+        }
     }
 
     async deleteRow(req, res) {
-        const { Identity } = req.body;
-        const result = await sheets_service.deleteRow(0, Identity);
-        res.json(result);
+        const { spreadsheetId, pageName, param, value } = req.body;
+        const sheets_service = new SheetService(spreadsheetId, pageName);
+        const deleteRow = await sheets_service.deleteRow(param, value);
+        switch (deleteRow.status) {
+            case 'success':
+                res.status(200).json(deleteRow.message);
+                break;
+            case 'error':
+                res.status(500).json(deleteRow.message);
+                break;
+            default:
+                res.status(404).json(deleteRow.message);
+                break;
+        }
     }
 
     async updateRow(req, res) {
-        const { Identity } = req.body;
-        const updatedRow = await sheets_service.updateRow(
-            0,
-            Identity,
-            req.body
-        );
-        res.json(updatedRow);
+        const { spreadsheetId, pageName, param, value, newData } = req.body;
+        const sheets_service = new SheetService(spreadsheetId, pageName);
+        const updateRow = await sheets_service.updateRow(param, value, newData);
+        switch (updateRow.status) {
+            case 'success':
+                res.status(200).json(updateRow.message);
+                break;
+            case 'error':
+                res.status(500).json(updateRow.message);
+                break;
+            default:
+                res.status(404).json(updateRow.message);
+                break;
+        }
     }
 }
 
--- src/services/sheets-service.js
+++ src/services/sheets-service.js
@@ -1,5 +1,4 @@
 const { authenticate } = require('../providers/google-sheets-provider');
-const { sheets } = require('../config');
 
 class SheetService {
     constructor(spreadsheetId, pageName) {
@@ -9,150 +8,167 @@ class SheetService {
     }
 
     async getAll() {
-        const document = await this.authenticatedDocument;
-        const { spreadsheetId, pageName } = this;
-        if (document) {
-            const range = pageName;
-            const response = await document.spreadsheets.values.get({
-                spreadsheetId,
-                range
-            });
-
-            const rows = response.data.values;
-            const headers = rows[0];
-            const data = rows.slice(1);
-
-            // Converter os dados em um array de objetos JSON
-            const jsonData = data.map((row) => {
-                const rowData = {};
-                headers.forEach((header, index) => {
-                    rowData[header] = row[index];
+        try {
+            const document = await this.authenticatedDocument;
+            const { spreadsheetId, pageName } = this;
+            if (document) {
+                const range = pageName;
+                const response = await document.spreadsheets.values.get({
+                    spreadsheetId,
+                    range
                 });
-                return rowData;
-            });
-            console.log(jsonData);
-            return jsonData;
+
+                const rows = response.data.values;
+                const headers = rows[0];
+                const data = rows.slice(1);
+
+                // Converter os dados em um array de objetos JSON
+                const jsonData = data.map((row) => {
+                    const rowData = {};
+                    headers.forEach((header, index) => {
+                        rowData[header] = row[index];
+                    });
+                    return rowData;
+                });
+                return { status: 'success', message: jsonData };
+            }
+        } catch (error) {
+            return { status: 'error', message: error };
         }
     }
 
     async saveData(newData) {
-        const document = await this.authenticatedDocument;
-        const { spreadsheetId, pageName } = this;
-        if (document) {
-            const range = pageName;
-            const response = await document.spreadsheets.values.append({
-                spreadsheetId,
-                range,
-                valueInputOption: 'RAW',
-                resource: {
-                    values: [Object.values(newData)]
-                }
-            });
-            console.log(response);
-            return response;
+        try {
+            const document = await this.authenticatedDocument;
+            const { spreadsheetId, pageName } = this;
+            if (document) {
+                const range = pageName;
+                await document.spreadsheets.values.append({
+                    spreadsheetId,
+                    range,
+                    valueInputOption: 'RAW',
+                    resource: {
+                        values: [Object.values(newData)]
+                    }
+                });
+                return { status: 'success', message: 'Salvo com sucesso!' };
+            }
+        } catch (error) {
+            return { status: 'error', message: error };
         }
     }
 
     async getRowByParam(param, value) {
-        const document = await this.authenticatedDocument;
-        const { spreadsheetId, pageName } = this;
-        if (document) {
-            const range = pageName;
-            const response = await document.spreadsheets.values.get({
-                spreadsheetId,
-                range
-            });
-            const rows = response.data.values;
-            const headers = rows[0];
-            const data = rows.slice(1);
-            const rowIndex = data.findIndex(
-                (row) => row[headers.indexOf(param)] === value
-            );
-            if (rowIndex !== -1) {
-                const row = data[rowIndex];
-                const rowData = {};
-                headers.forEach((header, index) => {
-                    rowData[header] = row[index];
+        try {
+            const document = await this.authenticatedDocument;
+            const { spreadsheetId, pageName } = this;
+            if (document) {
+                const range = pageName;
+                const response = await document.spreadsheets.values.get({
+                    spreadsheetId,
+                    range
                 });
-                console.log(rowData);
-                return rowData;
+                const rows = response.data.values;
+                const headers = rows[0];
+                const data = rows.slice(1);
+                const rowIndex = data.findIndex(
+                    (row) => row[headers.indexOf(param)] === value
+                );
+                if (rowIndex !== -1) {
+                    const row = data[rowIndex];
+                    const rowData = {};
+                    headers.forEach((header, index) => {
+                        rowData[header] = row[index];
+                    });
+                    return { status: 'success', message: rowData };
+                }
+                return {
+                    status: 'warning',
+                    message: `Não foi encontrada uma linha com ${param} igual a ${value}`
+                };
             }
-            console.log(
-                `Não foi encontrada uma linha com ${param} igual a ${value}`
-            );
-            return null;
+        } catch (error) {
+            return { status: 'error', message: error };
         }
     }
 
     async deleteRowByParam(param, value) {
-        const document = await this.authenticatedDocument;
-        const { spreadsheetId, pageName } = this;
-        if (document) {
-            const range = pageName;
-            const response = await document.spreadsheets.values.get({
-                spreadsheetId,
-                range
-            });
-            const rows = response.data.values;
-            const headers = rows[0];
-            const data = rows.slice(1);
-            const rowIndex = data.findIndex(
-                (row) => row[headers.indexOf(param)] === value
-            );
-            if (rowIndex !== -1) {
-                const rangeToDelete = `${pageName}!${rowIndex + 2}:${rowIndex +
-                    2}`;
-                const deleteResponse = await document.spreadsheets.values.clear(
-                    {
+        try {
+            const document = await this.authenticatedDocument;
+            const { spreadsheetId, pageName } = this;
+            if (document) {
+                const range = pageName;
+                const response = await document.spreadsheets.values.get({
+                    spreadsheetId,
+                    range
+                });
+                const rows = response.data.values;
+                const headers = rows[0];
+                const data = rows.slice(1);
+                const rowIndex = data.findIndex(
+                    (row) => row[headers.indexOf(param)] === value
+                );
+                if (rowIndex !== -1) {
+                    const rangeToDelete = `${pageName}!${rowIndex +
+                        2}:${rowIndex + 2}`;
+                    await document.spreadsheets.values.clear({
                         spreadsheetId,
                         range: rangeToDelete
-                    }
-                );
-                console.log(deleteResponse);
-                return deleteResponse;
+                    });
+                    return {
+                        status: 'success',
+                        message: 'Deletado com sucesso!'
+                    };
+                }
+                return {
+                    status: 'warning',
+                    message: `Não foi encontrada uma linha com ${param} igual a ${value}`
+                };
             }
-            console.log(
-                `Não foi encontrada uma linha com ${param} igual a ${value}`
-            );
-            return null;
+        } catch (error) {
+            return { status: 'error', message: error };
         }
     }
 
     async updateRowByParam(param, value, newData) {
-        const document = await this.authenticatedDocument;
-        const { spreadsheetId, pageName } = this;
-        if (document) {
-            const range = pageName;
-            const response = await document.spreadsheets.values.get({
-                spreadsheetId,
-                range
-            });
-            const rows = response.data.values;
-            const headers = rows[0];
-            const data = rows.slice(1);
-            const rowIndex = data.findIndex(
-                (row) => row[headers.indexOf(param)] === value
-            );
-            if (rowIndex !== -1) {
-                const rangeToUpdate = `${pageName}!${rowIndex + 2}:${rowIndex +
-                    2}`;
-                const updateResponse = await document.spreadsheets.values.update(
-                    {
+        try {
+            const document = await this.authenticatedDocument;
+            const { spreadsheetId, pageName } = this;
+            if (document) {
+                const range = pageName;
+                const response = await document.spreadsheets.values.get({
+                    spreadsheetId,
+                    range
+                });
+                const rows = response.data.values;
+                const headers = rows[0];
+                const data = rows.slice(1);
+                const rowIndex = data.findIndex(
+                    (row) => row[headers.indexOf(param)] === value
+                );
+                if (rowIndex !== -1) {
+                    const rangeToUpdate = `${pageName}!${rowIndex +
+                        2}:${rowIndex + 2}`;
+                    await document.spreadsheets.values.update({
                         spreadsheetId,
                         range: rangeToUpdate,
                         valueInputOption: 'RAW',
                         resource: {
                             values: [Object.values(newData)]
                         }
-                    }
-                );
-                console.log(updateResponse);
-                return updateResponse;
+                    });
+                    return {
+                        status: 'success',
+                        message: 'Alterado com sucesso!'
+                    };
+                }
+                return {
+                    status: 'warning',
+                    message: `Não foi encontrada uma linha com ${param} igual a ${value}`
+                };
             }
-            console.log(
-                `Não foi encontrada uma linha com ${param} igual a ${value}`
-            );
-            return null;
+        } catch (error) {
+            return { status: 'error', message: error };
         }
     }
 }
