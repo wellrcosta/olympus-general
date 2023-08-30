c68eb51c2e4b8499d3fac44b2192e4cb4c5213f9
Wellington Reis <wellington.costa.97@gmail.com>
2023-08-30 14:19:43 -0300

    Added Swagger documentation on routes

--- src/routes/sheets-routes.js
+++ src/routes/sheets-routes.js
@@ -11,27 +11,156 @@ const {
 
 const routes = [];
 
+/**
+ *  @swagger
+ * /save:
+ *   post:
+ *     tags:
+ *     - "Sheets"
+ *     summary: "Save a new row"
+ *     description: "Save a new row in the sheet"
+ *     parameters:
+ *     - in: "body"
+ *       name: "body"
+ *       description: "Body example"
+ *       required: true
+ *       schema:
+ *         type: "object"
+ *         properties:
+ *           spreadsheetId:
+ *              type: "string"
+ *           pageName:
+ *              type: "string"
+ *           newData:
+ *              type: "object"
+ *     responses:
+ *       201:
+ *         description: "Created"
+ *       404:
+ *         description: "Bad request"
+ *       500:
+ *         description: "Internal Server Error"
+ */
 routes.push(
     new Route('/save', constants.HTTP2_METHOD_POST, sheetsController.saveRow, {
         body: save_data_body_schema
     })
 );
 
+/**
+ *  @swagger
+ * /getAll:
+ *   post:
+ *     tags:
+ *     - "Sheets"
+ *     summary: "Get All Rows"
+ *     description: "Get All Rows from the sheet"
+ *     parameters:
+ *     - in: "body"
+ *       name: "body"
+ *       description: "Body example"
+ *       required: true
+ *       schema:
+ *         type: "object"
+ *         properties:
+ *           spreadsheetId:
+ *              type: "string"
+ *           pageName:
+ *              type: "string"
+ *     responses:
+ *       200:
+ *         description: "OK"
+ *       404:
+ *         description: "Bad request"
+ *       500:
+ *         description: "Internal Server Error"
+ */
 routes.push(
     new Route(
-        '/getall',
+        '/getAll',
         constants.HTTP2_METHOD_POST,
         sheetsController.getAllRows,
         { body: get_all_body_schema }
     )
 );
 
+/**
+ *  @swagger
+ * /getSpecific:
+ *   post:
+ *     tags:
+ *     - "Sheets"
+ *     summary: "Get Specific Row"
+ *     description: "Get Specific Row from the sheet"
+ *     parameters:
+ *     - in: "body"
+ *       name: "body"
+ *       description: "Body example"
+ *       required: true
+ *       schema:
+ *         type: "object"
+ *         properties:
+ *           spreadsheetId:
+ *              type: "string"
+ *           pageName:
+ *              type: "string"
+ *           param:
+ *              type: "string"
+ *           value:
+ *              type: "string"
+ *     responses:
+ *       200:
+ *         description: "OK"
+ *       404:
+ *         description: "Bad request"
+ *       500:
+ *         description: "Internal Server Error"
+ */
 routes.push(
-    new Route('/getone', constants.HTTP2_METHOD_POST, sheetsController.getRow, {
-        body: get_row_by_param_body_schema
-    })
+    new Route(
+        '/getSpecific',
+        constants.HTTP2_METHOD_POST,
+        sheetsController.getRow,
+        {
+            body: get_row_by_param_body_schema
+        }
+    )
 );
 
+/**
+ *  @swagger
+ * /update:
+ *   post:
+ *     tags:
+ *     - "Sheets"
+ *     summary: "Update the entire row"
+ *     description: "Update the entire row in the sheet"
+ *     parameters:
+ *     - in: "body"
+ *       name: "body"
+ *       description: "Body example"
+ *       required: true
+ *       schema:
+ *         type: "object"
+ *         properties:
+ *           spreadsheetId:
+ *              type: "string"
+ *           pageName:
+ *              type: "string"
+ *           param:
+ *              type: "string"
+ *           value:
+ *              type: "string"
+ *           newData:
+ *              type: "object"
+ *     responses:
+ *       200:
+ *         description: "OK"
+ *       404:
+ *         description: "Bad request"
+ *       500:
+ *         description: "Internal Server Error"
+ */
 routes.push(
     new Route(
         '/update',
@@ -41,6 +170,38 @@ routes.push(
     )
 );
 
+/**
+ *  @swagger
+ * /delete:
+ *   post:
+ *     tags:
+ *     - "Sheets"
+ *     summary: "Delete the entire row"
+ *     description: "Delete a specific row in the sheet"
+ *     parameters:
+ *     - in: "body"
+ *       name: "body"
+ *       description: "Body example"
+ *       required: true
+ *       schema:
+ *         type: "object"
+ *         properties:
+ *           spreadsheetId:
+ *              type: "string"
+ *           pageName:
+ *              type: "string"
+ *           param:
+ *              type: "string"
+ *           value:
+ *              type: "string"
+ *     responses:
+ *       200:
+ *         description: "OK"
+ *       404:
+ *         description: "Bad request"
+ *       500:
+ *         description: "Internal Server Error"
+ */
 routes.push(
     new Route(
         '/delete',
